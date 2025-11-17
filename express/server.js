8716aa308b15c126484d6369ed0af76cfe3c0575
Maxvo <maxvo25@gmail.com>
2025-11-17 12:37:23 +1030

    update-

--- express/server.js
+++ express/server.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const path = require('path');
 const serverless = require('serverless-http');
-const twilio = require('twilio');
 const app = express();
 const bodyParser = require('body-parser');
 
@@ -15,14 +14,7 @@ router.get('/', (req, res) => {
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
-router.post('/sms', (req, res) => {
-  const twiml = new twilio.twiml.MessagingResponse();
-  twiml.message('hello world');
-  res.type('text/xml').send(twiml.toString());
-});
-
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
